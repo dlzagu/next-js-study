6492d5da0bdf473f6b88de45eeeddca2f1e5e8b4
dlzagu <eodnsdlekd@naver.com>
2023-04-03 03:47:50 +0900

    feat: Error handling

--- blog/pages/api/post/write.js
+++ blog/pages/api/post/write.js
@@ -1,88 +1,18 @@
-import Layout from '../../components/Layout'
-import { useEffect, useRef, useState } from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import { createPost } from '../../../lib/posts'
+import { format } from 'date-fns'
 
-// export async function getServerSideProps() {
-//   return {}
-// }
+export default async function handler(req, res) {
+  const { id, title, content } = req.body
 
-export default function Write() {
-  const router = useRouter()
-
-  useEffect(() => {
-    console.log(router.query)
-  }, [router.query])
-
-  const idRef = useRef(undefined)
-  const titleRef = useRef(undefined)
-  const contentRef = useRef(undefined)
-
-  const [showLink, setShowLink] = useState(false)
-
-  const handleSumbit = (event) => {
-    event.preventDefault()
-
-    const id = idRef.current.value
-    const title = titleRef.current.value
-    const content = contentRef.current.value
-
-    if (id && title && content) {
-      fetch('/api/post/write', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          id,
-          title,
-          content,
-        }),
-      })
-        .then((response) => {
-          if (response.ok) {
-            return response.json()
-          }
-          throw new Error('Fetch Error')
-        })
-        .then((data) => {
-          setShowLink(true)
-          alert(data.message)
-        })
-        .catch((error) => alert(`request error: ${error}`))
-    }
+  try {
+    await createPost({
+      id,
+      title,
+      date: format(new Date(), 'yyyy-MM-dd'),
+      content,
+    })
+    res.status(200).json({ message: 'create success' })
+  } catch (error) {
+    res.status(500).json({ error: `create failed ${error}` })
   }
-  return (
-    <Layout>
-      <h1>Write a post</h1>
-      <form onSubmit={handleSumbit}>
-        <input type="text" name="id" placeholder="id" required ref={idRef} />
-        <br />
-        <input
-          type="text"
-          name="title"
-          placeholder="title"
-          required
-          ref={titleRef}
-        />
-        <br />
-        <textarea
-          type="text"
-          name="content"
-          placeholder="content"
-          required
-          ref={contentRef}
-        />
-        <br />
-        <input type="submit" value="Create" />
-      </form>
-      {showLink && (
-        <Link href={`/posts/${idRef.current.value}`}>
-          <a>Created Post</a>
-        </Link>
-      )}
-    </Layout>
-  )
 }
-
-// Write.getInitialProps = async () => {
-//   return {}
-// }
